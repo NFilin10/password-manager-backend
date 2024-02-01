2abd743052f6f81b8325ca66aca1df153cdc5a5c
Nikita Filin <nikita10filin@gmail.com>
2024-02-02 00:20:27 +0200

    password adding implemented

--- src/controllers/passwordController.js
+++ src/controllers/passwordController.js
@@ -1,5 +1,4 @@
-
-const express = require('express')
+const express = require('express');
 const pool = require("../database");
 
 const getPasswords = async (req, res) => {
@@ -10,6 +9,23 @@ const getPasswords = async (req, res) => {
     res.json(course.rows);
 }
 
+const addPassword = async (req, res) => {
+    console.log("BODY:", req.body);
+    const data = req.body;
+
+    try {
+        await pool.query(
+            "INSERT INTO passwords (service_name, link, login, password) VALUES ($1, $2, $3, $4)",
+            [data.website, data.webLink, data.login, data.password]
+        );
+        res.status(201).json({ message: 'Password added successfully' });
+    } catch (error) {
+        console.error('Error adding password:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
-    getPasswords
+    getPasswords,
+    addPassword
 };
--- src/routes/passwords.route.js
+++ src/routes/passwords.route.js
@@ -1,8 +1,10 @@
 
 const express = require('express')
-const { getPasswords } = require('../controllers/passwordController')
+const { getPasswords, addPassword } = require('../controllers/passwordController')
 const router = express.Router()
 
 router.get('/', getPasswords)
+router.post('/add', addPassword)
+
 
 module.exports = router;
